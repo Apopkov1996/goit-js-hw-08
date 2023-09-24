cf6604579c1337c9794d84810b91d86ec68dd673
Apopkov1996 <antonpopkov113@gmail.com>
2023-09-24 18:24:25 +0300

    update task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,20 +1,30 @@
 import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
-const iframeEl = document.querySelector('#vimeo-player')
+const iframeEl = document.querySelector('#vimeo-player');
+const key = "videoplayer-current-time";
+const player = new Player(iframeEl)
 
-
-const player = new Player('handstick', {
-    id: 19231868,
-    width: 640
-});
-
-player.on('play', function() {
-    console.log('played the video!');
-});
-
-const onPlay = function(data) {
-    // data is an object containing properties specific to that event
+const onPlay = function (e) {
+    localStorage.setItem(key, e.seconds)
 };
 
-player.on('play', onPlay);
+player.on('timeupdate', throttle(onPlay, 1000));
+
+player.setCurrentTime(localStorage.getItem(key));
+
+
+// const player = new Player('handstick', {
+//     id: 19231868,
+//     width: 640
+// });
+
+// player.on('play', function() {
+//     console.log('played the video!');
+// });
+
+// const onPlay = function(data) {
+//     // data is an object containing properties specific to that event
+// };
+
+// player.on('play', onPlay);
