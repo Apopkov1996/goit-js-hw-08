93928f1e89dd648b2fdbbe8ea1b8a88618e729f3
Apopkov1996 <antonpopkov113@gmail.com>
2023-09-26 00:23:31 +0300

    final

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -12,19 +12,3 @@ const onPlay = function (e) {
 player.on('timeupdate', throttle(onPlay, 1000));
 
 player.setCurrentTime(localStorage.getItem(key));
-
-
-// const player = new Player('handstick', {
-//     id: 19231868,
-//     width: 640
-// });
-
-// player.on('play', function() {
-//     console.log('played the video!');
-// });
-
-// const onPlay = function(data) {
-//     // data is an object containing properties specific to that event
-// };
-
-// player.on('play', onPlay);
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,44 @@
+import throttle from "lodash.throttle";
+
+const refs = {
+  inputNameEl: document.querySelector('input[name="email"]'),
+  inputMessageEl: document.querySelector('textarea[name="message"]'),
+  formEl: document.querySelector('.feedback-form'),
+};
+
+refs.inputNameEl.addEventListener('input', throttle(onDataInput, 500))
+refs.inputMessageEl.addEventListener('input', throttle(onDataInput, 500));
+refs.formEl.addEventListener('submit', onFormSubmit)
+
+const key = "feedback-form-state";
+let data = {};
+
+function onDataInput(e) {
+  const name = e.target.name;
+  const value = e.target.value;
+  data[name] = value;
+  localStorage.setItem(key, JSON.stringify(data));
+}
+
+function onFormSubmit (e) {
+  e.preventDefault();
+  console.log(data);
+  e.target.reset();
+  localStorage.removeItem(key);
+  data = {};
+};
+
+
+function restorPage() {
+  data = JSON.parse(localStorage.getItem(key)) || {};
+  console.log(data);
+  
+  refs.inputNameEl.value = data.email || '';
+  refs.inputMessageEl.value = data.message || '';
+}
+restorPage();
+
+
+
+
 
